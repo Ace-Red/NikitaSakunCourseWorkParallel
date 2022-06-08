1f75e03cc0055b09f5a7afadabcd0583c003c08a
Nikita Sakun <NikitaSakun1@gmail.com>
2022-06-08 22:49:35 +0300

    Beta 1.0

---
+++ src/Algorithm.java
@@ -0,0 +1,108 @@
+import java.util.Random;
+import java.util.concurrent.CountDownLatch;
+
+public class Algorithm {
+
+    public static void main(String[] args) {
+        int matrixSize = 7000;
+        int threads = 1;
+        Random r = new Random();
+        double [][] matrixA = new double[matrixSize][matrixSize+1];
+
+        // Randomly generate matrix
+        for (int i = 0; i < matrixSize; i++) {
+            for (int j = 0; j < matrixSize+1; j++) {
+                matrixA[i][j] = r.nextDouble();
+            }
+        }
+        try {
+
+            if (threads < 1) {
+                System.out.println("The number of threads was adjusted to be 1");
+                System.out.println("can't perform any operations on <1 thread");
+                System.exit(1);
+            }
+            if (matrixA.length <= 1) {
+                System.err.println("The matrix size was <= [1x1]");
+                System.exit(1);
+            }
+            final double begin = System.nanoTime();
+            double[] result = doParallelGaussianElimination(matrixA, threads);
+            final double duration = (System.nanoTime() - begin) / 1000000;
+            System.out.println("\n" + duration + "ms");
+            // Rounding up the answers
+            //Arrays.stream(result)
+                    //.forEach(value -> System.out.printf("%.2f%n", value));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static double[] doParallelGaussianElimination(double[][] A, int numOfThreadsToUse) {
+        //printMatrix(A);
+
+        int availableProcessors = Runtime.getRuntime().availableProcessors();
+        System.out.println("Total processors: " + availableProcessors);
+        if (numOfThreadsToUse < availableProcessors) {
+            availableProcessors = numOfThreadsToUse;
+        }
+        if (availableProcessors > A.length) {
+            availableProcessors = A.length / 2;
+        }
+        // availableProcessors = 1;
+        System.out.println("Threads spawned for processing: " + availableProcessors);
+        int div = A.length / availableProcessors;
+        int remainder = A.length % availableProcessors;
+        Worker[] threads = new Worker[availableProcessors];
+        CountDownLatch latch = new CountDownLatch(availableProcessors);
+        for (int p = 0; p < availableProcessors; p++) {
+            int lower = p * div;
+            int higher = (p + 1) * div;
+            if (p == (availableProcessors - 1))
+                threads[p] = new Worker(A, lower, higher + remainder, latch);
+            else
+                threads[p] = new Worker(A, lower, higher, latch);
+            threads[p].start();
+        }
+        try {
+            for (Thread t : threads) {
+                t.join();
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Issue with Thread join! ");
+            e.printStackTrace();
+        }
+
+        //printMatrix(A);
+
+        return backSubstitution(A);
+    }
+
+    public static void printMatrix(double[][] answer) {
+        for (double[] doubles : answer) {
+            for (int j = 0; j < answer[0].length; j++) {
+                if (j == answer[0].length - 1)
+                    System.out.println(" || " + doubles[j]);
+                else
+                    System.out.print(doubles[j] + " ");
+            }
+            //System.out.println();
+        }
+        //System.out.println();
+    }
+
+    private static double[] backSubstitution(double[][] A) {
+
+        int n = A.length;
+        double[] currentArray = new double[n];
+        for (int x = n - 1; x >= 0; x--) {
+            currentArray[x] = A[x][n];
+            for (int y = x + 1; y < n; y++) {
+                currentArray[x] = currentArray[x] - A[x][y] * currentArray[y];
+            }
+            currentArray[x] = currentArray[x] / A[x][x];
+        }
+        return currentArray;
+    }
+
+}
---
+++ src/Worker.java
@@ -0,0 +1,73 @@
+import java.util.concurrent.CountDownLatch;
+
+
+public class Worker extends Thread {
+    private final CountDownLatch latch;
+    private double[][] A;
+    private int startRow;
+    private final int endRow;
+
+
+    public Worker(double[][] A, int startRow, int endRow, CountDownLatch latch) {
+        this.A = A;
+        this.startRow = startRow;
+        this.endRow = endRow;
+        this.latch = latch;
+    }
+    /**
+     * Swaps rows
+     * Accessed by the worker threads
+     *
+     * @param A : Coefficient Matrix
+     * @param i : first row to  be swapped
+     * @param j : second row to  be swapped
+     */
+    public static void swapRows(double[][] A, int i, int j) {
+        for (int k = 0; k <= A.length; k++) {
+            double temp = A[i][k];
+            A[i][k] = A[j][k];
+            A[j][k] = temp;
+        }
+    }
+    public void run() {
+        int n = A.length;
+        for (; startRow < endRow; startRow++) {
+            int pointer = startRow;
+            double value = A[pointer][startRow];
+            // The pivots exits on diagonal elements <ie> A[i][i]
+            // Pointer starting from that element to the last element in the matrix column
+            // Get maximum value in the diagonal column
+
+            for (int x = startRow + 1; x < endRow; x++) { //endrow--n
+                if (Math.abs(A[x][startRow]) > value) {
+                    value = A[x][startRow];
+                    pointer = x;
+                }
+            }
+
+            // latch.countDown();
+            // Make sure the current row has the max val. at the pivot position.
+            // Swap the origin row with MAX row
+
+            if (startRow != pointer) {
+                swapRows(A, startRow, pointer);
+            }
+            latch.countDown();
+            // Iterate through the rows
+            // f = A[row][c] / A[pivot][c]
+            // Perform subtraction with multiple on the entire row
+            // Populate the bottom triangular matrix with 0's
+            for (int x = startRow + 1; x < endRow; x++) { //
+                // Multiplier
+                double f = A[x][startRow] / A[startRow][startRow];
+                for (int y = startRow + 1; y <= n; y++)
+                    A[x][y] -= A[startRow][y] * f;
+                A[x][startRow] = 0;
+            }
+        }
+    }
+}
+
+
+
+
